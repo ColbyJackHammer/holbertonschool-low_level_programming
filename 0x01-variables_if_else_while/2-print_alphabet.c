fcd152a49e295d2fa77e03697b5b4448afa46a22
root <james.colby.austin@gmail.com>
2023-01-30 12:21:03 -0800

    changes to 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,12 @@
  */
 
 int main () {
-   char ch;
+char ch;
 
-   for(ch = 'a' ; ch <= 'z' ; ch++) {
-      putchar(ch);
-   }
-   putchar('\n');
+for(ch = 'a' ; ch <= 'z' ; ch++) {
+putchar(ch);
+}
+putchar('\n');
    
-   return(0);
+return(0);
 }
