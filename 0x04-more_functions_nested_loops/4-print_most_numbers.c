47ce09825c7885cd07b219e8aab08adb517e52b6
root <james.colby.austin@gmail.com>
2023-02-03 12:01:43 -0800

    changes to 3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  *print_most_numbers - write a function that prints the numbers from 0-9 followed by a 
+  *new line.
+  *
+  *Do not print 2 or 4
+  *Return: void.
+  */
+
+
