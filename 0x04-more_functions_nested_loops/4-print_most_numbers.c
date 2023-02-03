d5dd38e581ea657b4e90fc1778967c47f3b48b9d
root <james.colby.austin@gmail.com>
2023-02-03 12:27:53 -0800

    changes to 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
-  *print_most_numbers - write a function that prints the numbers from 0-9 followed by a 
-  *new line.
+  *print_most_numbers - write a function that prints the numbers
+  *from 0-9 followed by a new line.
   *
   *Do not print 2 or 4
   *Return: void.
   */
 
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if ((i == 2) || (i == 4))
+		{
+			continue;
+		}
+		_putchar('0' + i);
+	}
+_putchar('\n');
+}
 
